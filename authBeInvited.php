4341e2dbb7f9df8f3649e17e9bb5df1031423a7e
lclichen <lclichen@mail.ustc.edu.cn>
2023-11-21 23:25:54 +0800

    授权-邀请功能

---
+++ authBeInvited.php
@@ -0,0 +1,43 @@
+<?php
+header("content-type:text/html;charset=utf-8");
+include_once __DIR__ . "/common.php";
+$data = initPostData();
+$token = $data['token'];
+$seccs = $data['scene'];
+
+$con = pdo_database();
+if ($token) {
+    [$openid, $identity, $nickName] = pdoCheckUserPrivilege($con, $token);
+    //var_dump([$token,$openid,$identity,$nickName]);
+}
+
+if ($openid && $identity == 'u') {
+    $identity = pdoCheckCatEditPrivilege($con, $openid, $id);
+}
+
+$ntime = date("Y-m-d h:i:sa");
+
+$sth = $con->prepare("SELECT * FROM invitepower WHERE secret_checksum = :seccs");
+$sth->execute(array(':seccs' => $seccs));
+$resu = $sth->fetch(PDO::FETCH_ASSOC);
+if($resu){
+    if ($resu['invite_period_time']>$ntime && $resu['times_left'] > 0) {
+        $sthi = $con->prepare('INSERT INTO userpower (catid,openid,power,auth_by) VALUES (:id, :openid, "e", :authby)', array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $result = $sthi->execute(array(':id' => $resu['catid'], ':openid' => $openid, ':authby' => $resu['inviter_openid']));
+        if($resu['times_left']>1){
+            $con->prepare("UPDATE invitepower SET times_left = times_left - 1 WHERE secret_checksum = :seccs ")->execute(array(':seccs' => $seccs));
+        } else {
+            $con->prepare("DELETE FROM invitepower WHERE secret_checksum = :seccs")->execute(array(':seccs' => $seccs));
+        }
+    } else {
+        $con->prepare("DELETE FROM invitepower WHERE secret_checksum = :seccs")->execute(array(':seccs' => $seccs));
+    }
+    if($result){
+        $redata['code'] = 10;
+    }
+    echo json_encode($redata, JSON_UNESCAPED_UNICODE);
+    $con = null;
+}else{
+    $con = null;
+    die('{"code":1005,"msg":"邀请码已失效！"}');
+}
---
+++ authInvite.php
@@ -0,0 +1,36 @@
+<?php
+header("content-type:text/html;charset=utf-8");
+include_once __DIR__ . "/common.php";
+$data = initPostData();
+$catid = (int)$data['catid'];
+$token = $data['token'];
+$period_days = (int)$data['pdays'];
+$times_left = (int)$data['tleft'];
+
+$con = pdo_database();
+if ($token) {
+    [$openid, $identity, $nickName] = pdoCheckUserPrivilege($con, $token);
+    //var_dump([$token,$openid,$identity,$nickName]);
+}
+
+if ($openid && $identity == 'u') {
+    $identity = pdoCheckCatEditPrivilege($con, $openid, $id);
+}
+$redata = array();
+if ($identity == 'a' || $identity == 'o' || $identity == 's') {
+    $sth = $con->prepare("INSERT INTO invitepower (catid,inviter_openid,invite_create_time,invite_period_time,times_left,secret_checksum) VALUES (:catid,:openid,:ntime,:ptime,:times_left,:seccs)");
+    $ntime = date("Y-m-d h:i:sa");
+    $ptime = date("Y-m-d h:i:sa", strtotime("+$period_days day"));
+    $seccs = md5($openid.$ntime.$catid);
+    $result = $sth->execute(array(':catid'=>$catid,':openid' => $openid,':ntime'=>$ntime,':ptime'=>$ptime,'times_left'=>$times_left,':seccs'=>$seccs));
+    if($result){
+        $redata['code'] = 10;
+        $redata['scene'] = $seccs;
+    }
+    echo json_encode($redata, JSON_UNESCAPED_UNICODE);
+    $con = null;
+    return;
+}
+
+$con = null;
+die('{"code":1002,"msg":"请重新登录！"}');
